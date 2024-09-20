dca811ec45517288b149d8a9da950cfe4e41ae19
tanjilalom <tanjil15-3079@diu.edu.bd>
2024-09-20 18:48:11 +0600

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:todo_app_with_firebase/auth/login_screen.dart';
+import 'package:todo_app_with_firebase/auth/signup_screen.dart';
 import 'package:todo_app_with_firebase/screen/home_screen.dart';
 import 'package:todo_app_with_firebase/screen/splash_screen.dart';
 
 Future main() async {
-  SystemChrome.setSystemUIOverlayStyle(
-    const SystemUiOverlayStyle(
-      statusBarColor: Colors.transparent,
-      statusBarIconBrightness: Brightness.dark,
-    ),
-  );
   WidgetsFlutterBinding.ensureInitialized();
   try {
     await Firebase.initializeApp();
@@ -36,11 +30,12 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: SplashScreen(),
+      home: const SplashScreen(),
 
       routes: {
         '/home': (context) => HomeScreen(),
         '/login': (context) => LoginScreen(),
+        '/signup': (context) => SignupScreen(),
       },
     );
   }
