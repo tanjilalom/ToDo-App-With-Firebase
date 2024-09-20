e97434838c20ebf541b14cdd71f20d95207756f9
tanjilalom <tanjil15-3079@diu.edu.bd>
2024-09-20 18:38:00 +0600

    login page

--- lib/auth/login_screen.dart
+++ lib/auth/login_screen.dart
@@ -1,11 +1,11 @@
-import 'package:firebase/auth/firebase_auth/firebase_auth_services.dart';
-import 'package:firebase/auth/signup_screen.dart';
-import 'package:firebase/widget/button_widget.dart';
-import 'package:firebase/widget/textfield_widget.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:todo_app_with_firebase/auth/firebase_auth/firebase_auth_services.dart';
+import 'package:todo_app_with_firebase/auth/signup_screen.dart';
+import 'package:todo_app_with_firebase/widget/button_widget.dart';
+import 'package:todo_app_with_firebase/widget/textfield_widget.dart';
 
 class LoginScreen extends StatefulWidget {
   LoginScreen({super.key});
