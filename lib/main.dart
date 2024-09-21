19384d689daff7a5e0a18276213ab70442630260
tanjilalom <tanjil15-3079@diu.edu.bd>
2024-09-21 12:31:04 +0600

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:todo_app_with_firebase/auth/login_screen.dart';
 import 'package:todo_app_with_firebase/auth/signup_screen.dart';
@@ -30,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const SplashScreen(),
+      home: LoginScreen(),
 
       routes: {
         '/home': (context) => HomeScreen(),
